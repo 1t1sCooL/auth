b1ab6c66964f1fbd3061f1bdac52c99be21b7543
1t1sCooL <mmalabugin@gmail.com>
2026-02-07 21:44:37 +0300

    add secrets validation

--- be/src/config/constants.js
+++ be/src/config/constants.js
@@ -1,6 +1,8 @@
 const JWT_SECRET = process.env.JWT_SECRET;
-if (!JWT_SECRET || JWT_SECRET.length < 32) {
-  console.error("❌ JWT_SECRET должен быть задан в .env и иметь длину не менее 32 символов");
+if (!JWT_SECRET || JWT_SECRET.length < 29) {
+  console.error(
+    "❌ JWT_SECRET должен быть задан в .env и иметь длину не менее 29 символов",
+  );
   process.exit(1);
 }
 
